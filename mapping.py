025324d838504385960240e059971d7ce07cf802
Aayush Sharma <aayushramansharma@gmail.com>
2025-02-17 13:26:45 +0100

    imports

---
+++ mapping.py
@@ -0,0 +1,11 @@
+import time
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
+
